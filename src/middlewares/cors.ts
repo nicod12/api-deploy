9ef50de032ccce458f831a2d81941d73c6695f06
nicod12 <dumenicolas45@gmail.com>
2023-09-20 12:27:01 -0300

    upd back

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 const ACCEPTED_ORIGINS = [
     'http://localhost:8080',
     'http://localhost:3000',
+    'http://localhost:5173'
   ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
